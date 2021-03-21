94d54166c316c872bdcfa04cdcd2337ed4d5c377
Ruslan <russlik88@gmail.com>
2021-03-21 20:35:46 +0100

    Created pagination on fetched data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+/** @format */
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
+import TableHeader from './TableHeader';
+import Pagination from './Pagination';
+// import useFullPageLoader from 'hooks/useFullPageLoader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [comments, setComments] = useState([]);
+	const [totalItems, setTotalItems] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
+	const ITEMS_PER_PAGE = 50;
+
+	const headers = [
+		{ name: 'No#', field: 'id' },
+		{ name: 'Name', field: 'name' },
+		{ name: 'Email', field: 'email' },
+		{ name: 'Comment', field: 'body' },
+	];
+
+	useEffect(() => {
+		const getData = () => {
+			fetch('https://jsonplaceholder.typicode.com/comments')
+				.then((response) => response.json())
+				.then((json) => {
+					setComments(json);
+				});
+		};
+
+		getData();
+	}, []);
+
+	const commentsData = useMemo(() => {
+		let computedComments = comments;
+
+		setTotalItems(computedComments.length);
+		//Current Page slice
+		return computedComments.slice(
+			(currentPage - 1) * ITEMS_PER_PAGE,
+			(currentPage - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE
+		);
+	}, [comments, currentPage]);
+
+	return (
+		<div className="App">
+			<Pagination
+				total={totalItems}
+				itemsPerPage={ITEMS_PER_PAGE}
+				currentPage={currentPage}
+				onPageChange={(page) => setCurrentPage(page)}
+			/>
+			<input type="text" placeholder="Search" />
+			<table className="table table-striped">
+				<TableHeader headers={headers} />
+				<tbody>
+					{commentsData.map((comment) => (
+						<tr>
+							<th scope="row">{comment.id}</th>
+							<th>{comment.name}</th>
+							<th>{comment.email}</th>
+							<th>{comment.body}</th>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/TableHeader.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from 'react';
+
+const TableHeader = ({ headers }) => {
+	return (
+		<thead>
+			<tr>
+				{headers.map((head) => (
+					<th key={head.field}>{head.name}</th>
+				))}
+			</tr>
+		</thead>
+	);
+};
+
+export default TableHeader;
